00dc550c1fc20271a758dc91fcd1d7588b3bd985
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-02-06 15:52:44 -0500

    add session.id attribute to POST routes for posts and comments; connect server to public folder and handlebar templates

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const controllers = require('./controllers');
 const sequelize = require("./config/connection");
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: process.env.SESS_SECRET,
+    secret: 'secret',
     cookie: {
         maxAge: 2 * 60 * 60 * 1000
     },
@@ -19,8 +23,14 @@ const sess = {
     })
 };
 
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(controllers);
 
