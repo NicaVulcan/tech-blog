a4905f9c90a104a70cbb15812cdfb01b30f64f19
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-02-07 16:06:13 -0500

    add dashboard routes and renders for dashboard and edit-post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,13 +1,98 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-    res.render('dashboard')
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+        .then(postData => {
+            const posts = postData.map(post => post.get({ plain: true }));
+            res.render('dashboard', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.get('/edit/:id', (req, res) => {
-    res.render('edit-post')
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']   
+            },
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+            }
+        ]
+    })
+        .then(postData => {
+            if (!postData) {
+                res.status(404).json({
+                    message: 'Post not found!'
+                });
+                return;
+            }
+            const post = postData.get({ plain: true });
+            res.render('edit-post', {
+                post, 
+                loggedIn: req.session.loggedIn
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
