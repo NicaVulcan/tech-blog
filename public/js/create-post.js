d9965f4fa09665d623644c0fd040480d8b78f0a9
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-02-07 17:31:48 -0500

    add comment functionality

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -2,7 +2,7 @@ async function addPostHandler (event) {
     event.preventDefault();
 
     const title = document.querySelector('#post-title').value.trim();
-    const content = document.querySelector('#post-content').value;
+    const content = document.querySelector('#post-content').value.trim();
 
     const response = await fetch('/api/posts', {
         method: 'POST',
